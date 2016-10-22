39dc34bab4bd8dfa97dd17e8398a0877d0cc7979
Lappy\Raziel <e@e.com>
2016-10-22 16:13:16 +0300

    Frontend concept

---
+++ Frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lawApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lawApp
+ */
+angular.module('lawApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Frontend/app/scripts/controllers/main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lawApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lawApp
+ */
+angular.module('lawApp')
+  .controller('MainCtrl', function ($scope, $http) {
+  	$scope.q = 'Search';
+    $scope.jsonator = [];
+  	//$scope.jsonator = [{name: 'AAR-SP', value: 26}, {name:'Duminica', value: 26}, {name:'Euripide', value: 90}];
+
+
+  	$scope.qRequest = function(){
+  		$http({
+  			method: 'GET',
+  			url: '/search?q=' + $scope.q
+		  }).then(function successCallback(response) {
+    		// this callback will be called asynchronously
+    		// when the response is available
+    		$scope.jsonator = response;
+
+
+
+    		console.log(response);
+
+  		}, function errorCallback(response) {
+    		// called asynchronously if an error occurs
+    		// or server returns response with an error status.
+    		console.log(response);
+  		});
+
+  		console.log($scope.q);
+  	}
+  });
