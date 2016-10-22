b95d299606dd5c0ef9fd73f6b17df273c9d878de
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-22 11:29:47 +0300

    Initial django setup

---
+++ legi/site/settings/example_local.py
@@ -0,0 +1,15 @@
+import os
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+SECRET_KEY = FILL_THIS_OUT
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jshacks-legi',
+    }
+}
+
