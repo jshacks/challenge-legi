9a92c6f6bdb9d8d779c2227b962b83fa9c34ae70
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-22 12:42:24 +0300

    add models

--- legi/models.py
+++ legi/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+
+class SubmittedData(models.Model):
+    sha1 = models.CharField(max_length=50, primary_key=True)
+    data = JSONField(default=dict, blank=True)
+    first_submit = models.DateTimeField(auto_now=True)
+    last_submit = models.DateTimeField(auto_now_add=True)
+
+
+class DocumentFile(models.Model):
+    sha1 = models.CharField(max_length=50, primary_key=True)
+    filename = models.CharField(max_length=512)
+    size = models.IntegerField()
+    content_type = models.CharField(max_length=128, blank=True)
+
+
+class TikaCache(models.Model):
+    sha1 = models.CharField(max_length=50, primary_key=True)
+    data = JSONField(default=dict, blank=True)
+    date = models.DateTimeField(auto_now=True)
+
+
+class Document(models.Model):
+    file_url = models.CharField(max_length=512, primary_key=True)
+    submit = models.ForeignKey('SubmittedData')
+    file = models.ForeignKey('DocumentFile')
+    parsed = JSONField(default=dict, blank=True)
+    date = models.DateTimeField(auto_now=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
