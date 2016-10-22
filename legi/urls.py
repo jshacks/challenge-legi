ec659c94d9e87517ad0b28f561bc3c755f8dfd28
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2016-10-22 12:17:01 +0300

    add all views

--- legi/urls.py
+++ legi/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^search$', views.search),
+    url(r'^doc/(?P<doc_id>[\-_A-Za-z0-9]+)$', views.doc),
+    url(r'^submit$', views.submit),
+    url(r'^status$', views.status),
+    url(r'^$', views.index),
 ]
--- legi/views.py
+++ legi/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import Http404, JsonResponse, HttpResponseBadRequest, HttpResponseServerError, HttpResponse
+from django.views.decorators.http import require_GET, require_POST
 
-# Create your views here.
+
+@require_GET
+def search(request):
+    q = request.GET.get('q')
+    if not q:
+        return HttpResponseBadRequest("q not set")
+
+    document_ids = [1]
+
+    data = {
+        'status': 'ok',
+        'document_ids': document_ids,
+        'results': len(document_ids),
+    }
+
+    return JsonResponse(data)
+
+
+@require_GET
+def index(request):
+    return HttpResponse("index")
+
+
+@require_GET
+def status(request):
+
+    data = {
+        'status': 'ok',
+        'num_documents': 0,
+        'num_requests': 0,
+    }
+    return JsonResponse(data)
+
+
+@require_GET
+def doc(request, doc_id):
+    data = {
+        'status': 'ok',
+        'id': doc_id,
+    }
+    return JsonResponse(data)
+
+
+@require_POST
+def submit(request):
+    response = {
+        'status': 'ok',
+        'new': True,
+    }
+    return JsonResponse(response)
