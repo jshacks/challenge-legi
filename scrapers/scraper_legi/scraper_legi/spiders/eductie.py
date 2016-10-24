d0213ecf7cb94abeebf408f04e326fc1e3115af1
Eduard Voinea <edvoinea@gmail.com>
2016-10-24 19:49:33 +0300

    Update eductie.py
    'educatie' scraper functional

--- scrapers/scraper_legi/scraper_legi/spiders/eductie.py
+++ scrapers/scraper_legi/scraper_legi/spiders/eductie.py
@@ -1,4 +1,8 @@
 import scrapy
+import requests
+import json
+
+URL = 'http://legi.vij.ro:9337/submit'
 
 class EducatieSpider(scrapy.Spider):
     name = "educatie"
@@ -7,7 +11,18 @@ class EducatieSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for document in response.xpath('//ul//li//p | //li[count(a)=1]'):
-            yield {
-                'titlu': document.xpath('./a/text()').extract_first()
+        for document in response.xpath('/html/body/div[3]/div/div/div/div[2]/div/div[1]/div/div[1]/div/article/div[1]/div/div/ul//li'):
+            if document.xpath('./a'):
+                varPath = './a'
+            else:
+                varPath = './p/a'
+            data = {
+                'titlu': document.xpath(varPath + '/text()').extract_first() ,
+                'originalUrl': 'http://www.edu.ro' + document.xpath(varPath + '/@href').extract_first() ,
+                'minister': 'Ministerul Educatiei Nationale si Cercetarii Stiintifice' ,
+                'originalPage': response.url ,
+                'crawler': 'educatie' ,
             }
+            requests.post(URL, data={'data': json.dumps(data)})
+            yield data
+
